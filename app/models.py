ae6dbe506f2c0e532b11d0c57f5e06fde6f867e7
zordo <pedrogodinhoc@gmail.com>
2019-11-25 17:55:11 +0100

    Chap7

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from app import db
 from flask_login import UserMixin
 from app import login
-
+from hashlib import md5
 
 @login.user_loader 
 def load_user(id):
@@ -23,6 +23,15 @@ class User(UserMixin,db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash,password) 
+
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default= datetime.utcnow)
+
 
 
 class Post(db.Model):
